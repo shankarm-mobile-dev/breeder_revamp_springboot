190a06b20474c4839f6bcfcb30676eacdb29b84a
deepak-sg-dev <deepakkumar@sugunafoods.com>
2026-05-28 15:45:45 +0530

    Added Laying Report

---
+++ src/main/java/com/suguna/breeder_revamp/service/LayingReportService.java
@@ -0,0 +1,8 @@
+package com.suguna.breeder_revamp.service;
+
+import com.suguna.breeder_revamp.dto.LayingReportRequest;
+
+public interface LayingReportService  {
+    String  getLayingReport(LayingReportRequest req );
+
+}
