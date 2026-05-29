fc2627cf2ec99a2743a79111b98b7fafa7f5f32c
deepak-sg-dev <deepakkumar@sugunafoods.com>
2026-05-29 17:22:28 +0530

    Added body weight and 100 grd detail reports

---
+++ src/main/java/com/suguna/breeder_revamp/service/BodyWeightReportService.java
@@ -0,0 +1,8 @@
+package com.suguna.breeder_revamp.service;
+
+import com.suguna.breeder_revamp.dto.BodyWeightReportRequestDto;
+
+public interface BodyWeightReportService {
+
+    String  getBodyWeight(BodyWeightReportRequestDto req );
+}
---
+++ src/main/java/com/suguna/breeder_revamp/service/GrdReportService.java
@@ -0,0 +1,7 @@
+package com.suguna.breeder_revamp.service;
+
+import com.suguna.breeder_revamp.dto.GrdReportRequestDto;
+
+public interface GrdReportService {
+    String  getGrdReport(GrdReportRequestDto req );
+}
